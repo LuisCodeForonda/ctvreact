0c61102e66a9d05d902c89367bc03384f74ee6e3
Luis A. ctv <luis@gmail.com>
2023-08-30 21:04:09 -0400

    lauyout creado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import Counter from "./components/Counter"
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Programacion from './components/Programacion'
 
 function App() {
 
   return (
     <>
-     <h1 className='text-2xl'>Hellow</h1>
-     <Counter />
+      <h1 className='text-2xl'>Hellow</h1>
+      <Counter />
+      <Router>
+        <div className="App">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Inicio</Link>
+              </li>
+              <li>
+                <Link to="/about">Acerca de</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contacto</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   )
 }
---
+++ src/components/Programacion.jsx
@@ -0,0 +1,8 @@
+function Programacion(){
+
+    return(
+        <h1>Programacion</h1>
+    );
+}
+
+export default Programacion;
